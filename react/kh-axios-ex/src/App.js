15da5130b8c13c451a69fb88055e6e030ae5052b
kimfjd <kimfjd@naver.com>
2024-05-08 12:46:38 +0900

    리엑트연습용

--- react/kh-axios-ex/src/App.js
+++ react/kh-axios-ex/src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Axios from "./Axios";
+import NewsList from "./NewsList";
 function App() {
   return (
     <>
-      <Axios />
+      <NewsList></NewsList>
     </>
   );
 }
